831b7162128ebe6995944591772529bd445ed246
John Hamilton <john.hamilton.developer@gmail.com>
2021-09-13 11:51:12 +1000

    Set up basic player movement

---
+++ Moving-along/Assets/Scenes/characters/player/player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float speed = 10;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.position += moveAmount;
+    }
+
+}
